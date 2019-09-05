12a030a5d274d93539e17760b6efc367c8cc3dfa
Marlan McInnes-Taylor <contact@marlanmcinnestaylor.com>
2019-09-05 19:14:54 -0400

    added command prompt

--- shell.c
+++ shell.c
@@ -20,13 +20,78 @@ void addToken(instruction* instr_ptr, char* tok);
 void printTokens(instruction* instr_ptr);
 void clearInstruction(instruction* instr_ptr);
 void addNull(instruction* instr_ptr);
+//void parseCommand(char* token);
 
 int main()
 {
+	char command[100];
+	int exit = 0;
 
+	char* token = NULL;
+	char* temp = NULL;
 
+	instruction instr;
+	instr.tokens = NULL;
+	instr.numTokens = 0;
 
-    return 0;
+	while(exit != 1){
+		printf("%s@%s:%s> ", getenv("USER"), getenv("HOSTNAME"), getenv("PWD"));
+		//printf("Please enter an instruction: ");
+		//fgets(command, 100, stdin);
+		// loop reads character sequences separated by whitespace
+		do
+		{
+			scanf("%ms", &token);
+			//scans for next token and allocates token var to size of scanned token
+			temp = (char *)malloc((strlen(token) + 1) * sizeof(char));
+
+			int i;
+			int start = 0;
+			for (i = 0; i < strlen(token); i++)
+			{
+				//pull out special characters and make them into a separate token in the instruction
+				if (token[i] == '|' || token[i] == '>' || token[i] == '<' || token[i] == '&')
+				{
+					if (i - start > 0)
+					{
+						memcpy(temp, token + start, i - start);
+						temp[i - start] = '\0';
+						addToken(&instr, temp);
+					}
+
+					char specialChar[2];
+					specialChar[0] = token[i];
+					specialChar[1] = '\0';
+
+					addToken(&instr, specialChar);
+
+					start = i + 1;
+				}
+			}
+
+			if (start < strlen(token))
+			{
+				memcpy(temp, token + start, strlen(token) - start);
+				temp[i - start] = '\0';
+				addToken(&instr, temp);
+			}
+
+			//free and reset variables
+			free(token);
+			free(temp);
+
+			token = NULL;
+			temp = NULL;
+		} while ('\n' != getchar()); //until end of line is reached
+
+		addNull(&instr);
+		printTokens(&instr);
+		clearInstruction(&instr);
+
+		exit = 1;
+	}
+
+	return 0;
 }
 
 
@@ -80,3 +145,5 @@ void clearInstruction(instruction* instr_ptr)
 	instr_ptr->tokens = NULL;
 	instr_ptr->numTokens = 0;
 }
+
+//void parseCommand(char* token){}
